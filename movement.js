003279c3b1bf46bdf00004b01d3e4c921007c6c0
Kaos <kaoppenh@ucsc.edu>
2016-05-11 21:59:37 -0700

    bit of a start

---
+++ movement.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+function movement() {
+    //keycodes
+    var rightCode = 39;
+    var leftCode = 37;
+    var upCode = 38;
+    var downCode = 40;
+    var dCode = 68;
+    var aCode = 65;
+    var wCode = 87;
+    var sCode = 83;
+    var shiftCode = 16;
+}
