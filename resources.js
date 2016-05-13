af551f64ae823489b776f5267aa69f09bdb8547f
Jarrett vdB <jsvanden@ucsc.edu>
2016-05-13 14:17:57 -0700

    Added some art

--- resources.js
+++ resources.js
@@ -1,10 +1,12 @@
 // Sprites
 
 var spr_john = new Sprite('resources/john.jpg');
+var spr_room01 = new Sprite('resources/level01.png', {width:640, height:480});
+var spr_tempChar = new Sprite('resources/MoveDown.png', {width:62, height:88, yOffset:5});
 
 // Rigid Body
 
-var rb_character = new RigidBody({isDynamic: true});
+var rb_character = new RigidBody({isDynamic: true, height:55});
 
 // Scripts
 
@@ -14,10 +16,13 @@ var script_movement = new movement();
 // GameObjects
 
 var go_character = new GameObject();
-go_character.addComponent(spr_john, "sprite");
+go_character.addComponent(spr_tempChar, "sprite");
 go_character.addComponent(rb_character, "body");
 go_character.addComponent(script_movement, "movement");
 go_character.addComponent(script_sample, "Sample Script");
+
+var go_room01 = new GameObject();
+go_room01.addComponent(spr_room01, "sprite");
 
 /*
 var go_SampleGameObject = new GameObject();
--- scenes.js
+++ scenes.js
@@ -3,4 +3,5 @@ var MainMenu = new Scene("Main Menu");
 MainMenu.onLoad = function()
 {
   MainMenu.add(go_character, {x:320, y:240});
+  MainMenu.add(go_room01, {x:320, y:240, z:-2});
 }
