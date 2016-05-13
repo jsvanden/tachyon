2104eae0a8251de6f6399d0fcb5ce31a9d5262e4
Jarrett vdB <jsvanden@ucsc.edu>
2016-05-12 19:05:19 -0700

    Add Character

--- resources.js
+++ resources.js
@@ -1,18 +1,24 @@
 // Sprites
 
-var spr_john = new Sprite('resources/john.jpg', {width: 100, height: 100});
+var spr_john = new Sprite('resources/john.jpg');
 
 // Rigid Body
 
-var rb_test = new RigidBody({width: 200, height:200, radius:100, shape:"square"});
-var rb_test2 = new RigidBody({isTrigger:true, isDynamic:true, width: 200, height:200, radius:100, shape:"square"});
+var rb_character = new RigidBody({isDynamic: true});
 
 // Scripts
 
 var script_sample = new sampleScript();
+var script_movement = new movement();
 
 // GameObjects
 
+var go_character = new GameObject();
+go_character.addComponent(spr_john, "sprite");
+go_character.addComponent(rb_character, "body");
+go_character.addComponent(script_movement, "movement");
+
+/*
 var go_SampleGameObject = new GameObject();
 go_SampleGameObject.addComponent(spr_john, "john");
 //go_SampleGameObject.addComponent(script_sample, "consoleLog");
@@ -22,3 +28,4 @@ var go_SampleGameObject2 = new GameObject();
 go_SampleGameObject2.addComponent(spr_john, "john");
 go_SampleGameObject2.addComponent(rb_test2, "physics");
 go_SampleGameObject2.addComponent(script_sample, "consoleLog");
+*/
--- scenes.js
+++ scenes.js
@@ -1,4 +1,5 @@
 // Main Menu
 var MainMenu = new Scene("Main Menu");
-var johnCena = MainMenu.add(go_SampleGameObject, {x:400, y:240});
-var johnCena2 = MainMenu.add(go_SampleGameObject2, {x:20, y:400});
+//var johnCena = MainMenu.add(go_SampleGameObject, {x:400, y:240});
+//var johnCena2 = MainMenu.add(go_SampleGameObject2, {x:20, y:400});
+var character = MainMenu.add(go_character, {x:320, y:240});
