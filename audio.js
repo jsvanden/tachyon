940bd7ebf196a3a192b65642dc1311ab2ede071a
Thelonius Erickson <theoserickson@gmail.com>
2016-05-15 04:29:47 -0700

    added another track to audio.js be careful though, you may have a bad time
    A proof of concept to show support for more than two tracks plus a cool song. For the working prototype we need to comment it out as we dont have permission to use it. Just comment out the last addSong(). Just thought it would be something fun to showcase.

--- audio.js
+++ audio.js
@@ -48,8 +48,10 @@ var songNum = 0;
 var keyPressed = 0;
 var paused = true;
 var songs = [];
+var eggSeen = false;  //easter egg of sorts, set to true to disable 
 addSong("http://people.ucsc.edu/~tsericks/CMPM120/Tension.mp3")
 addSong("http://people.ucsc.edu/~tsericks/CMPM120/synthpop.mp3")//example songs
+addSong("http://people.ucsc.edu/~tsericks/CMPM120/BadTime.mp3")
 
 document.addEventListener("keypress",keyPress);
  
@@ -61,55 +63,56 @@ console.log("rate: "+rate);
 console.log("keyCode: "+e.keyCode);
 console.log("global var: "+gIter);
 */
-	keyPressed = e.keyCode
-	checkKey(keyPressed);
+keyPressed = e.keyCode
+checkKey(keyPressed);
 }    
 
 function pause(){
-	console.log("pause")
-	if(paused == false){audacity.pause()
-	}else{play()}
-    paused = !paused
+   console.log("pause")
+   if(paused == false){audacity.pause()
+   }else{play()}
+      paused = !paused
+      
+    if(songNum == 2 && eggSeen == false){alert("You feel your sins crawling down your back"),eggSeen = true, console.log("seen")};  
 }
 
 function stop(){
-	console.log("stop")
-	audacity.pause();
-	paused = true;
-	audacity.currentTime = 0;
+   console.log("stop")
+   audacity.pause();
+   paused = true;
+   audacity.currentTime = 0;
 }
 
 
 function play(){
-	console.log("play")
-	audacity.play();
+   console.log("play")
+   audacity.play();
 }
 
 function change(){
-	console.log("change")
-	if(songNum<songs.length-1){songNum++
-	}else{songNum = 0}
-	audacity.src = songs[songNum];
-	paused = true;
-	audacity.pause();
-	console.log(audacity.src);
+   console.log("change")
+   if(songNum<songs.length-1){songNum++
+   }else{songNum = 0}
+   audacity.src = songs[songNum];
+   paused = true;
+   audacity.pause();
+   console.log(audacity.src);   
 }
 function setRate(rate){
-	console.log("rate")
-	audacity.playbackRate = rate;
+   console.log("rate")
+	 audacity.playbackRate = rate;
 }
 function addSong(url){
-	songs.push(url);
+songs.push(url);
 }
 
 function selectSong(num){
-	songNum = num;
-	audacity.src = songs[songNum];
+songNum = num;
 }
 
 function loop(){
-	//setRate(rate);   //changes play speed
-	//console.log(audacity.playbackRate); //debug
+   //setRate(rate);   //changes play speed
+   //console.log(audacity.playbackRate); //debug
 }
 setInterval(loop,30);
 
@@ -120,58 +123,54 @@ setInterval(loop,30);
 
 // optional manual keyboard testing
 function checkKey(key){
-	switch (key){
-		case 0:   //space
-			console.log("space");           		
-			change();
-			break;  
-		case 27:   // escape
-			console.log("esc");
-			stop()
-			break;
-		case 13:
-			console.log("enter")
-			pause();
-			break;
-			 
-		case 38: 
-			console.log("up")
-			setRate(rate+=.1)
-			console.log(rate);
-			break;
-		case 37: 
-			console.log("left")
-			break;
-		case 39: 
-			console.log("right")
-			break;
-		case 40: 
-			console.log("down")
-			setRate(rate-=.1);
-			console.log(rate);
-			break;
-		
-		case 65: 
-			console.log("a")
-			break;
-		case 87: 
-			console.log("w")
-			setRate(rate+=.1)
-			console.log(rate);
-			break;
-		case 68: 
-			console.log("d")
-			break;
-		case 83:
-			console.log("s")
-			setRate(rate-=.1);
-			console.log(rate);
-			break;
-		 
-		default:
-			console.log(rate);
-			console.log(e.keyCode);
-	}
+ switch (key){
+   case 0:   //space
+      console.log("space");           		
+      change();
+      break;  
+   case 27:   // escape
+   	 console.log("esc");
+     stop()
+     break;
+   case 13:
+   	 console.log("enter")
+     pause();
+     break;
+     
+   case 38: 
+   	 console.log("up")
+     setRate(rate+=.1)
+     console.log(rate);
+     break;
+   case 37: 
+   	 console.log("left")
+     break;
+   case 39: 
+   	 console.log("right")
+     break;
+   case 40: 
+   	 console.log("down")
+     setRate(rate-=.1);
+     console.log(rate);
+     break;
+   
+   case 65: 
+   	 console.log("a")
+     break;
+   case 87: 
+   	 console.log("w")
+     break;
+   case 68: 
+   	 console.log("d")
+     break;
+   case 83:
+   	 console.log("s")
+     break;
+ 
+   default:
+		 console.log(rate);
+     console.log(e.keyCode);
+   }
 }//end checkKey 
 
 //space = 32 esc = 27   enter = 13
