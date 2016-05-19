b17c516d2d2dbeb55a7e35b8c70d25017e7b73a2
Jarrett vdB <jsvanden@ucsc.edu>
2016-05-19 00:49:47 -0700

    Demo Ready

--- game.js
+++ game.js
@@ -17,8 +17,8 @@ InputManager.add("enter", [13,32]);
 
 sceneManager.drawDebug = true;
 sceneManager.init();
-sceneManager.play("Level 1");
-//sceneManager.play("Main Menu");
+//sceneManager.play("Level 1");
+sceneManager.play("Main Menu");
 
 requestAnimFrame(sceneManager.run);
 
