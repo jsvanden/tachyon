6ca79579220ae80fc5ba70b2d8726404a512f498
Kaos <kaoppenh@ucsc.edu>
2016-05-30 14:59:58 -0700

    prototype piston script

---
+++ piston.js
@@ -0,0 +1,48 @@
+// JavaScript source code
+function piston() {
+    var base;
+    var origin;
+    var goal;
+    var speed = 1;
+    var clock;
+    var cap;
+    var player;
+    var spike;
+    var down = true;
+
+    this.start = function () {
+        player = Level_2.find("Main Character");
+        clock = player.getComponent("Movement").tier;
+        cap = player.getComponent("Movement").tierCap;
+        base = this.parent;
+        origin = base.y;
+        goal = origin + 60;
+        spike = base.parent.getComponent("Spikes");
+
+        spike.onCollision(other) = function () {
+            if (other.parent == player) {
+                //implement death
+            }
+        }
+    }
+
+    this.update = function () {
+        clock = player.getComponent("movement").tier;
+        cap = player.getComponent("movement").tierCap;
+
+        if (down) {
+            base.y += ((speed*cap)/clock);
+            if (base.y >= goal) {
+                base.y = goal;
+                down = false;
+            }
+        }
+        else {
+            base.y -= ((speed*cap)/clock);
+            if (base.y <= origin) {
+                base.y = origin;
+                down = true;
+            }
+        }
+    }
+}
